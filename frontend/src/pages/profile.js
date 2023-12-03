977461ca5727afadd05b43ed080f7f35eb6f2b19
jasper-nguyen <nguyenxjasper@icloud.com>
2023-12-02 23:31:43 -0800

    added the change password and username buttons

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -10,12 +10,34 @@ function Profile({ removeToken }){
         navigate('/');
     };
 
+    const navigatePass = useNavigate();
+    const changePagePass = () => {
+
+        navigate('/changePassword');
+      };
+
+    const navigateUser = useNavigate();
+    const changePageUser = () => {
+
+        navigate('/changeUsername');
+      };
+
     return (
         <div>
             <h1>Profile</h1>
+
             <button onClick={logMeOut}>Logout</button>
+            <button onClick = {changePagePass}>Edit Password</button>
+            <button onClick = {changePageUser}>Edit Username</button>
         </div>
     );
 }
+
+
+
+
+
+
+
 
 export default Profile;
