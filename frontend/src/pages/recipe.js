b61691b7c6dda0e5b24fe2dbd0d610398488ed2c
sZeru <kolby.salmond@yahoo.com>
2023-12-06 12:30:11 -0800

    fixed bug in recipe.js

--- frontend/src/pages/recipe.js
+++ frontend/src/pages/recipe.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { useNavigate, useLocation } from 'react-router-dom';
 import RecipeCard from './RecipeCard';
- 
-function Recipe(){
-    const location = useLocation();
-    const navigate = useNavigate();
-    const recipes = location.state?.recipes || [];
-    console.log(recipes)
-    const navigateBack = () => {
-      navigate(-1);
+
+function Recipe() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const recipes = location.state?.recipes || [];
+  console.log(recipes);
+
+  const navigateBack = () => {
+    navigate(-1);
   };
 
- 
   return (
     <div>
-        <button onClick={navigateBack}>Back</button>
-               <div className="recipes-container">
-<div className="recipes-container">
-      {recipes.map((recipe, index) => (
-        <div key={index}>
+      <button onClick={navigateBack}>Back</button>
+      <div className="recipes-container">
+        {recipes.map((recipe, index) => (
+          <div key={index}>
             <RecipeCard key={index} recipe={recipe} />
-        </div>
-      ))}
+          </div>
+        ))}
       </div>
     </div>
   );
